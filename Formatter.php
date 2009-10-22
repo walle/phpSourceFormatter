582172df917df0d420c8a4b84a1886d8f9e60dd9
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2009-10-22 05:01:19 +0000

    Removed Wv and resumed working on my version
    git-svn-id: https://svn.wallgren.me/phpSourceFormatter/trunk@5 145b29aa-556e-4ccf-81a6-020568ddd5d2

--- Formatter.php
+++ Formatter.php
@@ -27,160 +27,156 @@ class Formatter
     $in_at = false;
     $in_php = false;
 
+    $code = preg_replace('/[ \t]+/', ' ', $code);
+    $code = preg_replace('/['.PHP_EOL.']+/', PHP_EOL, $code);
+
     $result = '';
     $tokens = token_get_all ($code);
+
     foreach ($tokens as $token)
     {
+      $str = '';
+
       if (is_string ($token))
       {
-        $token = trim ($token);
+        $token = trim($token);
+
         if ($token == '{')
         {
-          $result = rtrim ($result) . PHP_EOL . $this->indenter->getIndention();
+          $str = $this->startLine($token);
           $this->indenter->indent();
-          $result .= $token . PHP_EOL . $this->indenter->getIndention();
         }
         elseif ($token == '}')
         {
           $this->indenter->outdent();
-          $result = rtrim ($result) . PHP_EOL . $this->indenter->getIndention() . $token . PHP_EOL . $this->indenter->getIndention();
+          $str = $this->endLine($token);
         }
         elseif ($token == ';')
         {
-          $result .= $token . PHP_EOL . $this->indenter->getIndention();
-        }
-        elseif ($token == ':')
-        {
-          $result .= $token . PHP_EOL . $this->indenter->getIndention();
-        }
-        elseif ($token == '(')
-        {
-          $result .= ' ' . $token;
-        }
-        elseif ($token == ')')
-        {
-          $result .= $token;
+          $str = $token.PHP_EOL;
         }
         elseif ($token == '@')
         {
           $in_at = true;
-          $result .= $token;
-        }
-        elseif ($token == '.')
-        {
-          $result .= ' ' . $token . ' ';
+          $str = $token;
         }
         elseif ($token == '=')
         {
-          $result .= ' ' . $token . ' ';
+          $str = ' ' . $token . ' ';
         }
         else
         {
-          $result .= $token;
+          $str = $token;
         }
       }
       else
       {
         list ($id, $text) = $token;
+
+        $text = trim($text);
+
         switch ($id)
         {
           case T_OPEN_TAG:
           case T_OPEN_TAG_WITH_ECHO:
             $in_php = true;
-            $result .= trim ($text);
+            $str = $this->endLine($text);
             break;
           case T_CLOSE_TAG:
             $in_php = false;
-            $result .= trim ($text);
+            $str = $this->startLine($text);
             break;
           case T_OBJECT_OPERATOR:
-            $result .= trim ($text);
+            $str = $text;
             $in_object = true;
             break;
           case T_STRING:
             if ($in_object)
             {
-              $result = rtrim ($result) . trim ($text);
+              $str = $text;
               $in_object = false;
             }
             elseif ($in_at)
             {
-              $result = rtrim ($result) . trim ($text);
+              $str = $text;
               $in_ = false;
             }
             else
             {
-              $result = rtrim ($result) . ' ' . trim ($text);
+              $str = ' ' . $text;
             }
             break;
           case T_ENCAPSED_AND_WHITESPACE:
           case T_WHITESPACE:
-            $result .= trim ($text);
+            $text = preg_replace('/[ \t]+/', ' ', $text);
+            $text = preg_replace('/['.PHP_EOL.']+/', PHP_EOL, $text);
+            $str .= $text;
             break;
-          case T_RETURN:
-          case T_ELSE:
-          case T_ELSEIF:
-            $result = rtrim ($result) . ' '  . trim ($text) . ' ';
-            break;
-          case T_CASE:
-          case T_DEFAULT:
-            $result = rtrim ($result) . PHP_EOL . $this->indenter->getIndention() . trim ($text) . ' ';
-            break;
-          case T_FUNCTION:
-          case T_CLASS:
-            $result .= PHP_EOL . $this->indenter->getIndention() . trim ($text) . ' ';
-            break;
-          case T_AND_EQUAL:
-          case T_AS:
-          case T_BOOLEAN_AND:
-          case T_BOOLEAN_OR:
-          case T_CONCAT_EQUAL:
-          case T_DIV_EQUAL:
-          case T_DOUBLE_ARROW:
-          case T_IS_EQUAL:
-          case T_IS_GREATER_OR_EQUAL:
-          case T_IS_IDENTICAL:
-          case T_IS_NOT_EQUAL:
-          case T_IS_NOT_IDENTICAL:
-          // case T_SMALLER_OR_EQUAL: // undefined constant ???
-          case T_LOGICAL_AND:
-          case T_LOGICAL_OR:
-          case T_LOGICAL_XOR:
-          case T_MINUS_EQUAL:
-          case T_MOD_EQUAL:
-          case T_MUL_EQUAL:
-          case T_OR_EQUAL:
-          case T_PLUS_EQUAL:
-          case T_SL:
-          case T_SL_EQUAL:
-          case T_SR:
-          case T_SR_EQUAL:
-          case T_START_HEREDOC:
-          case T_XOR_EQUAL:
-            $result = rtrim ($result) . ' ' . trim ($text) . ' ';
-            break;
-          case T_COMMENT:
-            $result = rtrim ($result) . PHP_EOL . $this->indenter->getIndention() . trim ($text) . ' ';
-            break;
-          case 'T_ML_COMMENT':
-            $result = rtrim ($result) . PHP_EOL;
+          case T_DOC_COMMENT:
             $lines = explode (PHP_EOL, $text);
+            $i = 0;
             foreach ($lines as $line)
             {
-              $result .= $this->indenter->getIndention() . trim ($line);
+              if ($i++ > 0)
+              {
+                $str.= ' ';
+              }
+              else
+              {
+                $str .= PHP_EOL;
+              }
+              $str .= $this->endLine($line);
+
             }
-            $result .= PHP_EOL;
             break;
-          case T_INLINE_HTML:
-            $result .= $text;
+          case T_CLASS:
+          case T_NAMESPACE:
+            $str = $this->startLine($text);
+            break;
+          case T_VARIABLE:
+            $str = ' '.$text;
             break;
           default:
-            $result .= trim ($text);
+            $str = $this->indenter->getStr(trim($text));
             break;
         }
       }
+
+      $result .= $str;
     }
 
     return $result;
+  }
+
+  private function startLine($str, $double = false)
+  {
+    $ret = '';
+    if ($double)
+    {
+      $ret .= PHP_EOL;
+    }
+
+    $ret .= PHP_EOL . $this->indenter->getStr(trim($str));
+
+    return $ret;
+  }
+
+  private function endLine($str, $double = false)
+  {
+    $ret = '';
+
+    $ret .= $this->indenter->getStr(trim($str)) . PHP_EOL;
+
+    if ($double)
+    {
+      $ret .= PHP_EOL;
+    }
+
+    return $ret;
+  }
+
+  private function ownLine($str)
+  {
+    return PHP_EOL.$this->indenter->getStr($str).PHP_EOL;
   }
 }
--- index.php
+++ index.php
@@ -71,6 +71,54 @@ class course
   public $errors;
 }';
 
+$service = '<?php
+class KursInfoWebServiceextends SoapClient
+{
+  /**
+ * @var array $classmap The defined classes
+ * @access private
+ */private static$classmap = array ("GetListCourses" => "GetListCourses","kursSokData" => "kursSokData","utbildningsForm" => "utbildningsForm","takt" => "takt","niva" => "niva","kursTid" => "kursTid","GetListCoursesResponse" => "GetListCoursesResponse","kurs" => "kurs","kursTillfalle" => "kursTillfalle","error" => "error","GetCourseDetails" => "GetCourseDetails","GetCourseDetailsResponse" => "GetCourseDetailsResponse","GetPeriodsWithCourses" => "GetPeriodsWithCourses","GetPeriodsWithCoursesResponse" => "GetPeriodsWithCoursesResponse","termin" => "termin");
+  /**
+ * @param string $wsdl The wsdl file to use
+ * @param array $config A array of config values
+ * @access public
+ */public function __construct ($wsdl = \'https://test.kursinfo.hj.se:444/webService/KursInfoWebService.asmx?wsdl\',$options = array ())
+  {
+    foreach ( self::$classmap as $key => $value)
+    {
+      if (!isset ($options[\'classmap\'][$key]))
+      {
+        $options[\'classmap\'][$key] = $value;
+      }
+    } parent:: __construct ($wsdl,$options);
+  }
+  /**
+ * @param GetListCourses $$parameters
+ * @access public
+ */public
+  function GetListCourses ( GetListCourses$parameters)
+  { return $this->__soapCall (\'GetListCourses\',array ($parameters));
+  }
+  /**
+ * @param GetCourseDetails $$parameters
+ * @access public
+ */public
+  function GetCourseDetails ( GetCourseDetails$parameters)
+  { return $this->__soapCall (\'GetCourseDetails\',array ($parameters));
+  }
+  /**
+ * @param GetPeriodsWithCourses $$parameters
+ * @access public
+ */public
+  function GetPeriodsWithCourses ( GetPeriodsWithCourses$parameters)
+  { return $this->__soapCall (\'GetPeriodsWithCourses\',array ($parameters));
+  }
+}';
+
 $formatter = new Formatter();
 
 print $formatter->formatString($source);
+
+print PHP_EOL.'-----'.PHP_EOL;
+
+print $formatter->formatString($service);
